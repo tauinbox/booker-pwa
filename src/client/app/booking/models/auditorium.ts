5fc21bab51fa71e19ceac62728e33c0bd7d3b572
Alexander Tupavov <alexander@tupavov.com>
2021-01-18 19:01:47 +0300

    code reformatted

--- src/client/app/booking/models/auditorium.ts
+++ src/client/app/booking/models/auditorium.ts
@@ -3,13 +3,13 @@ import { Seat } from './seat';
 export class Auditorium {
   [seatId: string]: string;
 
+  constructor(init: Auditorium) {
+    Object.assign(this, init);
+  }
+
   * [Symbol.iterator]?(): Generator<Seat> {
     for (const [seatId, userId] of Object.entries(this)) {
       yield {seatId, userId};
     }
-  }
-
-  constructor(init: Auditorium) {
-    Object.assign(this, init);
   }
 }
